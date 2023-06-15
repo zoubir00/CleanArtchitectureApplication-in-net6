3dd130bfc7d7f1cebfb1bb594d93d68e3f922afd
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-15 16:21:49 +0100

    Add controller and show data in swagger ui

---
+++ WebApi/Controllers/MembersController.cs
@@ -0,0 +1,25 @@
+﻿using Application;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController : ControllerBase
+    {
+        private readonly IMemberService _memberService;
+
+        public MembersController(IMemberService memberService)
+        {
+            _memberService = memberService;
+        }
+
+        [HttpGet("Get all members")]
+        public IActionResult GetAllMemebers()
+        {
+            var allMemebers = _memberService.GetAllMembers();
+            return Ok(allMemebers);
+        }
+    }
+}
