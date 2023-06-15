7a058ef315f1f7d11c4ab39cf94b41f089bddfe8
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-06-15 16:13:36 +0100

    set the structure of clean structure pattern

---
+++ Application/MemberService.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class MemberService : IMemberService
+    {
+        private readonly IMemberRepository _memberRepository;
+
+        public MemberService(IMemberRepository memberRepository)
+        {
+            _memberRepository = memberRepository;
+        }
+        public List<Member> GetAllMembers()
+        {
+            return _memberRepository.GetAllMembers();
+        }
+    }
+}
---
+++ Infrastructure/MemberRepository.cs
@@ -0,0 +1,27 @@
+﻿using Application;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class MemberRepository : IMemberRepository
+    {
+        public static List<Domain.Member> lstMember = new List<Member>()
+        {
+            new Domain.Member{  Id =1 ,Name= "Kirtesh Shah", Type ="G" , Address="Vadodara"},
+           new Domain.Member{  Id =2 ,Name= "Mahesh Shah", Type ="S" , Address="Dabhoi"},
+           new Domain.Member{  Id =3 ,Name= "Nitya Shah", Type ="G" , Address="Mumbai"},
+           new Domain.Member{  Id =4 ,Name= "Dilip Shah", Type ="S" , Address="Dabhoi"},
+           new Domain.Member{  Id =5 ,Name= "Hansa Shah", Type ="S" , Address="Dabhoi"},
+           new Domain.Member{  Id =6 ,Name= "Mita Shah", Type ="G" , Address="Surat"}
+        };
+        public List<Member> GetAllMembers()
+        {
+            return lstMember;
+        }
+    }
+}
